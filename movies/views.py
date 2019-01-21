5eacc1b1dce6f5756d7e30eecebe05cb9a6596a5
David Sampson <dcsampsonlaptop@gmail.com>
2019-01-21 22:23:24 +0000

    Done a bunch of css, nothing perfect but it's something

--- movies/views.py
+++ movies/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.db.models import Q
 
-from .models import Movies
+from .models import Movies, Genres
 
 def index(request):
-    movie_list = Movies.objects.order_by('-movieName')
+    movie_list = Movies.objects.order_by('overallRating')
+    genre_list = Genres.objects.order_by('genreName')
     template = 'movies/index.html'
     context = {
-        'movie_list': movie_list
+        'movie_list': movie_list,
+        'genre_list': genre_list
     }
     return render(request, template, context)
 
