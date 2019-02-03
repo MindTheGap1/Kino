f6e5621114550ff33c28f16bc152eef2bd66df74
David Sampson <dcsampsonlaptop@gmail.com>
2019-02-03 14:30:36 +0000

    Made all remaining tables in preparation

--- Kino/settings.py
+++ Kino/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'movies.apps.MovieConfig',
+    'account.apps.AccountConfig',
+    'orders.apps.OrderConfig',
 ]
 
 MIDDLEWARE = [
