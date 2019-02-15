6b7a02d2a8dd383f9ba575a2c2a0a18c11258e7c
David Sampson <dcsampsonlaptop@gmail.com>
2019-02-15 16:38:04 +0000

    Can now filter by actor, writer or director

--- movies/urls.py
+++ movies/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_id>/', views.detail, name='detail'),
     path('genre/<int:genre_id>/', views.genre_detail, name='genre'),
+    path('actor/<int:actor_id>/', views.actor_detail, name='actor'),
+    path('director/<int:director_id>/', views.director_detail, name='director'),
+    path('writer/<int:writer_id>/', views.writer_detail, name='writer'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -57,12 +57,67 @@ def genre_detail(request, genre_id):
 
     moviesQ = MovieGenres.objects.filter(genreId=genre_id)
     movie_list = Movies.objects.filter(genres__in=moviesQ).order_by('overallRating')
+
     genre_list = Genres.objects.order_by('genreName')
     search_form = SearchBar
 
     template = 'movies/genre_detail.html'
     context = {
         'selected_genre': selected_genre,
+        'movie_list': movie_list,
+        'genre_list': genre_list,
+        'search_form': search_form,
+    }
+    return render(request, template, context)
+
+def actor_detail(request, actor_id):
+    selected_actor = get_object_or_404(Actors, pk=actor_id)
+
+    moviesQ = MovieActors.objects.filter(actorId=actor_id)
+    movie_list = Movies.objects.filter(actors__in=moviesQ).order_by('overallRating')
+    
+    genre_list = Genres.objects.order_by('genreName')
+    search_form = SearchBar
+
+    template = 'movies/actor_detail.html'
+    context = {
+        'selected_actor': selected_actor,
+        'movie_list': movie_list,
+        'genre_list': genre_list,
+        'search_form': search_form,
+    }
+    return render(request, template, context)
+
+def director_detail(request, director_id):
+    selected_director = get_object_or_404(Directors, pk=director_id)
+
+    moviesQ = MovieDirectors.objects.filter(directorId=director_id)
+    movie_list = Movies.objects.filter(directors__in=moviesQ).order_by('overallRating')
+    
+    genre_list = Genres.objects.order_by('genreName')
+    search_form = SearchBar
+
+    template = 'movies/genre_detail.html'
+    context = {
+        'selected_director': selected_director,
+        'movie_list': movie_list,
+        'genre_list': genre_list,
+        'search_form': search_form,
+    }
+    return render(request, template, context)
+
+def writer_detail(request, writer_id):
+    selected_writer = get_object_or_404(Writers, pk=writer_id)
+
+    moviesQ = MovieWriters.objects.filter(writerId=writer_id)
+    movie_list = Movies.objects.filter(writers__in=moviesQ).order_by('overallRating')
+    
+    genre_list = Genres.objects.order_by('genreName')
+    search_form = SearchBar
+
+    template = 'movies/writer_detail.html'
+    context = {
+        'selected_writer': selected_writer,
         'movie_list': movie_list,
         'genre_list': genre_list,
         'search_form': search_form,
