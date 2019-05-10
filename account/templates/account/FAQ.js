bc331fa973917256e48b2ac08d2948b264a14ed5
David Sampson <dcsampsonlaptop@gmail.com>
2019-05-10 11:32:07 +0100

    Tidied up unnecessary files and added the static html files to repo (not integrated with site)

---
+++ account/templates/account/FAQ.js
@@ -0,0 +1,16 @@
+var buttons = document.getElementsByClassName("FAQ-button");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function () {
+
+  	this.classList.toggle('is-open');
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
